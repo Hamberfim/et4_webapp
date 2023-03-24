8935f84ffc5693d591b5fa0d9879046005e5a053
Hamberfim <adhamlingithub@gmail.com>
2023-03-24 11:09:53 -0500

    data modeling

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ module.exports = {
   output: { filename: "bundle.js" },
   resolve: { extensions: [".ts", ".js"] },
   module: {
-    rules: [{ test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }],
+    rules: [
+      { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
   },
   devServer: {
     static: "./assets",
